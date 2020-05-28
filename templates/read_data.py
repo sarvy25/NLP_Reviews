fb6a93fc0ae0b86432abfd8cc15e3ad652606923
Sarvenaz <sarvenaz.me@gmail.com>
2020-05-28 19:33:46 -0400

    Adding templates

---
+++ templates/read_data.py
@@ -0,0 +1,9 @@
+import os
+import pickle as pkl
+path = 'Users/sarvenaz/Documents/DataIncubator_sclpring2020Cohort'
+os.path.join(path, '/hotel_data_SF/InterContinental San Francisco')
+
+with open('meta.pkl','rb') as f:
+    data = pkl.load(f)
+    
+
